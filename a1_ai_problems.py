5c31b23f2b806ab657969946eb10d12c5776b0a4
Davin Nguyen <davin.nguyen2017@gmail.com>
2024-09-20 10:21:22 -0500

    ai_problems done

--- a1_ai_problems.py
+++ a1_ai_problems.py
@@ -17,3 +17,20 @@ assert fizbuzz(5) == "Buzz", "fizzbuzz 5 test"
 assert fizbuzz(6) == "Fizz", "fizzbuzz 6 test"
 assert fizbuzz(15) == "FizzBuzz", "fizzbuzz 15 test"
 
+def evenOdd(input_num):
+    if(input_num % 2 == 0):
+        return 'even'
+    else:
+        return 'odd'
+
+def SumofNumbers(input_num):
+    total_sum = sum(range(1, input_num + 1))
+    return total_sum
+
+def max(n1, n2, n3):
+    if(n1 >= n2 and n1 >= n3):
+        return n1
+    elif(n2 >= n1 and n2 >= n3):
+        return n2   
+    else:
+        return n3
