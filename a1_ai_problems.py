6ef5850c36acd8b71a3a12f75207d7af141de718
Davin Nguyen <davin.nguyen2017@gmail.com>
2024-09-20 10:25:28 -0500

    ai problems done

--- a1_ai_problems.py
+++ a1_ai_problems.py
@@ -17,15 +17,21 @@ assert fizbuzz(5) == "Buzz", "fizzbuzz 5 test"
 assert fizbuzz(6) == "Fizz", "fizzbuzz 6 test"
 assert fizbuzz(15) == "FizzBuzz", "fizzbuzz 15 test"
 
+# 1. Even or Odd: Write a program that asks the user for a number and then prints whether the number is even or odd.
+
 def evenOdd(input_num):
     if(input_num % 2 == 0):
         return 'even'
     else:
         return 'odd'
 
+# 2. Sum of Numbers: Create a program that calculates the sum of all numbers from 1 to a given number (inclusive) provided by the user.
+
 def SumofNumbers(input_num):
     total_sum = sum(range(1, input_num + 1))
     return total_sum
+
+# 3. Maximum of Three Numbers: Create a function that takes three numbers as arguments and returns the largest of the three.
 
 def max(n1, n2, n3):
     if(n1 >= n2 and n1 >= n3):
